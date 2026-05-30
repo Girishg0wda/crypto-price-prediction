114e881132aaa8d2eb1fe5c28d9cd09f37d9513d
Girish <girishgowdasr428@gmail.com>
2026-05-30 15:59:49 +0530

    Debug imports

--- app/app.py
+++ app/app.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
-st.title("Deployment Test")
+st.write("Step 1")
 
-st.success("App is running successfully!")
+import pandas as pd
+st.write("Step 2")
+
+import joblib
+st.write("Step 3")
+
+from ta.momentum import RSIIndicator
+st.write("Step 4")
+
+from ta.trend import MACD
+st.write("Step 5")
+
+st.success("All imports successful!")
